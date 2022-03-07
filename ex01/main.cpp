7b99fb7ec64bbf8e4cf1367306a0299eb1912fd4
Gundul <sampah@mailbox.org>
2022-03-07 11:43:29 +0100

    add ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,24 +6,24 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 13:56:11 by graja             #+#    #+#             */
-/*   Updated: 2022/03/06 15:11:11 by graja            ###   ########.fr       */
+/*   Updated: 2022/03/07 11:26:34 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "serials.hpp"
 
 static
-uintptr_t	*serialize(t_data *data)
+uintptr_t	serialize(t_data *data)
 {
 	std::cout << std::endl << "now serializing" << std::endl << std::endl;
-	return (reinterpret_cast<uintptr_t *>(data));
+	return (reinterpret_cast<uintptr_t >(data));
 }
 
 static
-t_data		*deserialize(uintptr_t *data)
+t_data		*deserialize(uintptr_t raw)
 {
 	std::cout << std::endl << "now deserializing" << std::endl << std::endl;
-	return (reinterpret_cast<t_data *>(data));
+	return (reinterpret_cast<t_data *>(raw));
 }
 
 static
@@ -39,7 +39,8 @@ void		printit(t_data	*data)
 int main(void)
 {
 	t_data			*test = new t_data;
-	uintptr_t	*hold;
+	t_data			*reint = NULL;
+	uintptr_t	raw;
 
 	test->name = "Hubert";
 	test->fname = "Mueller";
@@ -48,12 +49,14 @@ int main(void)
 	test->postcode = 19228;
 	test->city = "Bommelhausen";
 	printit(test);
-	hold = serialize(test);
-	std::cout << "data pointer:" << test << std::endl;
-	std::cout << "serial pointer:" << hold << std::endl;
-	test = deserialize(hold);
-	std::cout << "serial pointer:" << hold << std::endl;
-	std::cout << "data pointer:" << test << std::endl;
-	printit(test);
+	raw = serialize(test);
+	std::cout << "Pointer Adress: " << test << std::endl;
+	std::cout << "raw serialized: " << raw << std::endl;
+	reint = deserialize(raw);
+	std::cout << "New deserialized pointer Adress: " << reint << std::endl;
+	std::cout << "Old original pointer Adress    : " << test << std::endl;
+	std::cout << "Printout from new pointer:" << std::endl << std::endl;
+	printit(reint);
+
 	delete test;
 }
